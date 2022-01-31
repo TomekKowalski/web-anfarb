4eff5073c7d65bcf4242c516d8af8cb7b698ff20
TomekKowalski <t.kowalski7411@gmail.com>
2022-01-31 10:14:11 +0100

    edycja 2 script 31.01.2022

--- js_web/script.js
+++ js_web/script.js
@@ -1,4 +1,4 @@
-
+var yScrollAxis02 = 0;
 
 window.onload = function()
 {  
@@ -61,12 +61,67 @@ window.onload = function()
             //test.innerHTML += "<br>Suma: " + suma_metrow;
             tekst_suma_metrow.innerHTML = suma_metrow + " m";        
         };
+        
+        
+        ///////////wyswietlanie miniatur  ////////////////////////
+        
+        var tdNazwyWzorow = [];
+
+        var n = 0;
+        while(document.getElementById("td_wzor"+n+""))
+        {
+            tdNazwyWzorow.push(document.getElementById("td_wzor"+n+""));  ///pobranie kolejnych okienek tabliczki
+            n++;
+        }
+        for(var i=0; i<tdNazwyWzorow.length; i++)
+        {
+            tdNazwyWzorow[i].addEventListener("mouseout", zmienKolor);
+            tdNazwyWzorow[i].addEventListener("mouseover", wrocKolor);  
+            tdNazwyWzorow[i].addEventListener("mousemove", wyswietl_miniature_wzoru);
+
+        }
+        window.onscroll = function()
+        {       
+            yScrollAxis02 = window.pageYOffset;
+        };
+        
+        
     }catch(e){}
     
     //////////////////////////////////////////////////////////////////////////
        
     
 };
+
+function zmienKolor()
+{
+    this.className = "";    
+    var miniatura_zdjecia = document.getElementById("miniatura_zdjecia");
+    
+    miniatura_zdjecia.style.display = "none";
+}
+
+function wrocKolor()
+{       
+    this.className = "divNowyColor";      
+}
+
+function wyswietl_miniature_wzoru(event)
+{
+    var e = event || window.event;
+    this.className = "divNowyColor";
+    //var pozycjaScroll = e.pageYOffset;
+    
+      
+    var miniatura_zdjecia = document.getElementById("miniatura_zdjecia");
+    
+    miniatura_zdjecia.style.display = "block";
+    miniatura_zdjecia.style.left = e.clientX + 5 + "px";
+    miniatura_zdjecia.style.top = e.clientY + 5 + yScrollAxis02 + "px";
+    miniatura_zdjecia.innerHTML = "<img src='../WZORY_JPG_WSZYSTKIE/" + this.innerHTML + ".jpg' alt='Loading...' class='wielkosc_miniatura'/>\n\
+                                <br /><figcaption>"+this.innerHTML+"</figcaption>";
+       
+}
 
 
 
