338591bf51ca659003a3a7fca394ba861b283508
TomekKowalski <t.kowalski7411@gmail.com>
2022-02-02 21:05:38 +0100

    edycja 1 statystyki artykul 02.02.2022

--- wzory_od_klienta.php
+++ wzory_od_klienta.php
@@ -22,7 +22,7 @@
             <link rel="Stylesheet" media="print" type="text/css" href="dodruku.css" />
             <link href="css_wyglad_strony/style.css" rel="stylesheet" type="text/css"> 
             <link href="css_wyglad_strony/mobile_style.css" rel="stylesheet" type="text/css">
-    <title>Zamówienia drukarnia</title>
+		<title>Zamówienia drukarnia</title>
         <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
         <script src="//code.jquery.com/jquery-1.10.2.js"></script>
         <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
