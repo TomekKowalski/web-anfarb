96cd133f014cf8010c704a4f2164044bfa6f11b1
TomekKowalski <t.kowalski7411@gmail.com>
2022-02-23 18:11:59 +0100

    edycja 1 statystyki anfarb 23.02.2022

---
+++ wybierz_odbiorce.kontener_statystyki_anfarb.php
@@ -0,0 +1,56 @@
+<?php
+
+session_start();
+
+?>
+
+<TABLE cellpadding = '0'  cellspacing = '0' border = '0' width=100% height=100%>
+    <tr>
+        <td align=left>
+            <TABLE cellpadding = '0'  cellspacing = '0' border = '0' width=50% height=100%>
+               <tr>
+                   <td height = 30>
+                       <B><font class='opis_paneli'>STATYSTYKI AN-FARB</font></B><br>
+                   </td>
+               </tr>
+            </TABLE>
+        </td>
+    </tr>
+    <tr>
+        <td height = 10px>
+        </td>
+    </tr>
+    <tr>
+        <td align=center>           
+                        <TABLE>
+                            <tr>
+                                <td>
+                                    <a href="php_statystyki/statystyki_klient_anfarb.php" class="button">Klient An-Farb</a>
+                                </td>                              
+                                <td>
+                                    <a href="php_statystyki/statystyki_artykul_anfarb.php" class="button">Artykuły An-Farb</a>
+                                </td> 
+                                <!--
+                                <td>
+                                    <a href="php_statystyki/wykres_dnia.php" class="button">Wykres dnia</a>
+                                </td> 
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a href="php_statystyki/wykres_latami.php" class="button">Wykres rok</a>
+                                </td>                              
+                                <td>
+                                    <a href="php_statystyki/wykres_miesiace.php" class="button">Miesiące</a>
+                                </td>   
+                                <td>
+                                    <a href="php_statystyki/wykres_dnia.php" class="button">Wykres dnia</a>
+                                </td> 
+                                -->
+                            </tr>
+                        </TABLE>                  
+        </td>   
+    </tr>
+</TABLE>
+
+
+
